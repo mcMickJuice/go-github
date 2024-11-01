b13fd4fb279b8d4b2a7a2877ea1b75cfc95db0a1
Mike Joyce <mike.joyce@shipt.com>
2024-11-01 13:11:34 -0400

    pass token, call search repo endpoint

--- github/client.go
+++ github/client.go
@@ -13,30 +13,37 @@ import (
 //   -H "X-GitHub-Api-Version: 2022-11-28" \
 //   https://api.github.com/orgs/ORG/repos
 
-type RepoResponse struct {
+type SearchRepoResponse struct {
+TotalCount int `json:"total_count"`
+  Items []SearchRepoResponseItem
+}
+
+type SearchRepoResponseItem struct {
 	Id     int    `json:"id"`
 	NodeId string `json:"node_id"`
 	Name   string `json:"name"`
 }
 
 // fetch all repos available to user
-func FetchRepos(baseUrl string) ([]string, error) {
-	url := fmt.Sprintf("%s/orgs/shipt/repos", baseUrl)
+func FetchRepos(baseUrl, token string) ([]string, error) {
+  url := fmt.Sprintf("%s/search/repositories", baseUrl)
 	request, _ := http.NewRequest(http.MethodGet, url, nil)
 	client := &http.Client{}
+	request.Header.Add("X-GitHub-Api-Version", "2022-11-28")
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 	resp, err := client.Do(request)
 	defer resp.Body.Close()
 	if err != nil || resp.StatusCode != 200 {
 		return nil, err
 	}
 	body, err := io.ReadAll(resp.Body)
-	repoResponse := []RepoResponse{}
+	repoResponse := SearchRepoResponse{}
 	json.Unmarshal(body, &repoResponse)
 	if err != nil {
 		return nil, err
 	}
 	var repoNames []string
-	for _, repo := range repoResponse {
+	for _, repo := range repoResponse.Items{
 		repoNames = append(repoNames, repo.Name)
 	}
 	return repoNames, nil
--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"mj/go-github/github"
+	"os"
 )
 
 const rootApiUrl = "https://api.github.com"
+const apiTokenName = "GITHUB_READ_API_TOKEN"
+
 func main() {
 	fmt.Println("Hello!")
-	repoNames, err := github.FetchRepos(rootApiUrl)
+	token, exists := os.LookupEnv(apiTokenName)
+	if !exists {
+		fmt.Printf("Error: %s not found", apiTokenName)
+		return
+	}
+	repoNames, err := github.FetchRepos(rootApiUrl, token)
 
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
