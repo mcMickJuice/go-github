f1d2d0b567c38e4b806eec115aba4af0a55334cc
Mike Joyce <mike.joyce@shipt.com>
2024-11-04 19:54:16 -0500

    add cmd line args

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"mj/go-github/github"
 	"os"
@@ -9,16 +10,24 @@ import (
 const rootApiUrl = "https://api.github.com"
 const apiTokenName = "GITHUB_READ_API_TOKEN"
 
-// TODO - parallelize search calls, accept contributor and since Date, github query builder
 func main() {
-	fmt.Println("Calling Github...")
 	token, exists := os.LookupEnv(apiTokenName)
+
 	if !exists {
 		fmt.Printf("Error: %s not found", apiTokenName)
 		return
 	}
 	client := github.NewGithubClient(token, rootApiUrl)
-	prOverview, err := client.FetchContributions("dcheng1290", "2024-08-01")
+	user := flag.String("user", "", "Github User Handle")
+	from := flag.String("from", "2024-08-01", "Contributions since Date YYYY-MM-DD. default: 2024-08-01")
+  flag.Parse()
+
+	if *user == "" {
+		fmt.Println("user not provided")
+		return
+	}
+  fmt.Printf("Fetching Github Info for %s from %s\n", *user, *from)
+	prOverview, err := client.FetchContributions(*user, *from)
 
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
