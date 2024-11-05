35f1fb42f6b2157335362b0861bda96887734c33
Mike Joyce <mike.joyce@shipt.com>
2024-11-05 10:07:10 -0500

    wire up template to main

--- main.go
+++ main.go
@@ -20,20 +20,23 @@ func main() {
 	client := github.NewGithubClient(token, rootApiUrl)
 	user := flag.String("user", "", "Github User Handle")
 	from := flag.String("from", "2024-08-01", "Contributions since Date YYYY-MM-DD. default: 2024-08-01")
-  flag.Parse()
+	flag.Parse()
 
 	if *user == "" {
 		fmt.Println("user not provided")
 		return
 	}
-  fmt.Printf("Fetching Github Info for %s from %s\n", *user, *from)
+	fmt.Printf("Fetching Github Info for %s from %s\n", *user, *from)
 	prOverview, err := client.FetchContributions(*user, *from)
 
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 	}
 
-	fmt.Printf("PR Count: %d\nReview Count: %d\n", len(prOverview.PullRequests), len(prOverview.Reviews))
+	if err := github.RenderGithubContribution(os.Stdout, prOverview); err != nil {
+		fmt.Println("There was an error rendering contributions")
+		fmt.Println(err)
+	}
 
 	fmt.Println("Program has completed")
 }
